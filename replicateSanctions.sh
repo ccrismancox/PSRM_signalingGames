27bfdd3f484cb119ed547ddcf210e3d02a67f70a
Casey Crisman-Cox <ccrismancox@gmail.com>
2019-10-14 14:25:54 -0500

    clean slate

--- replicateSanctions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python CMLE_estimation.py > estimationLog.txt 2>&1
-Rscript estimation.R >> estimationLog.txt 2>&1
-Rscript standardErrors.R >> estimationLog.txt 2>&1
-Rscript comparativeStatics.R >> estimationLog.txt 2>&1 #outputs figure 
